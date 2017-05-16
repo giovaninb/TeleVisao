221707da046a3e02ab2c2fa6067c7869b1187d3d
Giovani Nicolas Bettoni <Giovani Nicolas Bettoni>
2017-05-16 12:02:34 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ufcspa/giovanib/televisao/LoginActivity.java
@@ -0,0 +1,55 @@
+package br.edu.ufcspa.giovanib.televisao;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText usuario;
+    private EditText senha;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
+
+    public void entrarOnClick(View v){
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
+
+        // TODO maneira de validar os campos
+        // TODO colocar email nos campos
+
+        // TODO consultar banco de dados para verificar login
+        if ("admin".equals(usuarioInformado) && "admin".equals(senhaInformada))
+        {
+            // vai para outra activity
+            String mensagemSucesso = getString(R.string.sucesso_login);
+            Toast toast = Toast.makeText(this, mensagemSucesso, Toast.LENGTH_SHORT);
+            toast.show();
+            startActivity(new Intent(this, DashboardActivity.class));
+
+        }
+        else {
+            // mostra mensagem de erro
+            String mensagemErro = getString(R.string.erro_login);
+            Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT);
+            toast.show();
+            limparCampos();
+        }
+    }
+
+    public void limparCampos() {
+        usuario.setText("");
+        senha.setText("");
+        usuario.requestFocus();
+    }
+}
---
+++ app/src/main/java/br/edu/ufcspa/giovanib/televisao/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package br.edu.ufcspa.giovanib.televisao;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handle = new Handler();
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarLogin();
+            }
+        }, 2000);
+    }
+
+    private void mostrarLogin() {
+        Intent intent = new Intent(SplashScreenActivity.this,
+                LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
